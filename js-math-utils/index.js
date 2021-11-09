6c55efafac3bf054303fc0db43ace40c5dde538a
Jose D. Vasquez <jodavaga@gmail.com>
2021-11-09 18:55:02 -0500

    Mi segunda funcion, resta

--- js-math-utils/index.js
+++ js-math-utils/index.js
@@ -2,3 +2,8 @@ export const sumar = (num1, num2) => {
   console.log(`a sumatoria es: ${num1 + num2}`);
   return num1 + num2;
 };
+
+export const resta = (num1, num2) => {
+  console.log(`La resta es: ${num1 - num2}`);
+  return num1 - num2;
+};
