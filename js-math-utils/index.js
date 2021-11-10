47722ce37ea2507b6a0d97f87967d681be2be070
Jose D. Vasquez <jodavaga@gmail.com>
2021-11-09 19:03:05 -0500

    Funcion muestra numero

--- js-math-utils/index.js
+++ js-math-utils/index.js
@@ -7,3 +7,11 @@ export const resta = (num1, num2) => {
   console.log(`La resta es: ${num1 - num2}`);
   return num1 - num2;
 };
+
+export const displayNumberByConsole = (number) => {
+  if (!number) return;
+  if (typeof number !== "number") return "Recuerda debes ingresar un numero";
+
+  console.log(`Su numero ingresado es: ${number}`);
+  return number;
+};
