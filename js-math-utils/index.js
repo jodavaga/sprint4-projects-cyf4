d9b2765454646fbc6cf41a16b3b80a1a312618f9
rudyminaya <rudy.minaya@copstone.net>
2021-11-11 18:52:32 -0500

    creando una función básica para identificar el menor número

--- js-math-utils/index.js
+++ js-math-utils/index.js
@@ -1,31 +1,39 @@
 export const sumar = (num1, num2) => {
-  console.log(`a sumatoria es: ${num1 + num2}`);
-  return num1 + num2;
-};
+    console.log(`a sumatoria es: ${num1 + num2}`)
+    return num1 + num2
+}
 
 export const resta = (num1, num2) => {
-  console.log(`La resta es: ${num1 - num2}`);
-  return num1 - num2;
-};
+    console.log(`La resta es: ${num1 - num2}`)
+    return num1 - num2
+}
 
 export const displayNumberByConsole = (number) => {
-  if (!number) return;
-  if (typeof number !== "number") return "Recuerda debes ingresar un numero";
+    if (!number) return
+    if (typeof number !== 'number') return 'Recuerda debes ingresar un numero'
 
-  console.log(`Su numero ingresado es: ${number}`);
-  return number;
-};
+    console.log(`Su numero ingresado es: ${number}`)
+    return number
+}
 
 export const getNumMayor = (num1, num2) => {
-  if (num1 > num2) {
-    return num1;
-  } else {
-    return num2;
-  }
-};
+    if (num1 > num2) {
+        return num1
+    } else {
+        return num2
+    }
+}
+
+export const getNumMenor = (n1, n2) => {
+    if (n1 < n2) {
+        return n1
+    } else {
+        return n2
+    }
+}
 
 export const getHigherNumberFromArray = (arrayNumbers) => {
-  return arrayNumbers.reduce((acc, valorActual) => {
-    return acc > valorActual ? acc : valorActual;
-  }, null);
-};
+    return arrayNumbers.reduce((acc, valorActual) => {
+        return acc > valorActual ? acc : valorActual
+    }, null)
+}
