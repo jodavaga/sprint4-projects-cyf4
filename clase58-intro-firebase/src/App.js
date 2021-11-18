304326f5e07e2dab72268faa9a2990de60235a39
Jose D. Vasquez <jodavaga@gmail.com>
2021-11-18 16:45:42 -0500

    fix: delete firebase code

--- clase58-intro-firebase/src/App.js
+++ clase58-intro-firebase/src/App.js
@@ -1,36 +1,14 @@
-import React, { useEffect } from "react";
+import React from "react";
 import logo from "./logo.svg";
-import { collection, addDoc } from "firebase/firestore";
-import { database } from "./firebase/config";
 import "./App.css";
 
-const usuario = {
-  name: "jose D.",
-  username: "jodavaga",
-  active: true,
-};
-
 function App() {
-  const handleClick = async () => {
-    try {
-      // creemos users
-      const docRef = await addDoc(collection(database, "usuarios"), usuario);
-      alert(`userAdded: ${docRef.id}`);
-      console.log("🚀 ~ ~ docRef", docRef.id);
-    } catch (e) {
-      console.log("🚀 ~ file: App.js ~ line 18 ~ handleClick ~ e", e);
-    }
-  };
-  useEffect(() => {}, []);
-
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Clase 58 - Introduccion a Firebase</p>
-        <span className="App-link" onClick={handleClick}>
-          Add New User to Firestore
-        </span>
+        <span className="App-link">Add New User to Firestore</span>
       </header>
     </div>
   );
