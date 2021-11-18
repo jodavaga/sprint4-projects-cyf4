4dffb88978ac5dc6311645c7b987a6d5ad0bcc02
Jose D. Vasquez <jodavaga@gmail.com>
2021-11-18 16:41:45 -0500

    feat: config, use firebase SDK 9

--- clase58-intro-firebase/src/App.js
+++ clase58-intro-firebase/src/App.js
@@ -1,22 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import logo from "./logo.svg";
+import { collection, addDoc } from "firebase/firestore";
+import { database } from "./firebase/config";
+import "./App.css";
+
+const usuario = {
+  name: "jose D.",
+  username: "jodavaga",
+  active: true,
+};
 
 function App() {
+  const handleClick = async () => {
+    try {
+      // creemos users
+      const docRef = await addDoc(collection(database, "usuarios"), usuario);
+      alert(`userAdded: ${docRef.id}`);
+      console.log("🚀 ~ ~ docRef", docRef.id);
+    } catch (e) {
+      console.log("🚀 ~ file: App.js ~ line 18 ~ handleClick ~ e", e);
+    }
+  };
+  useEffect(() => {}, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Clase 58 - Introduccion a Firebase</p>
+        <span className="App-link" onClick={handleClick}>
+          Add New User to Firestore
+        </span>
       </header>
     </div>
   );
