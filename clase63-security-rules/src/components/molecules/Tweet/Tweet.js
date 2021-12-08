6c42d467ac1e680783ae9609dbdb476cfad69b81
Jose D. Vasquez <jodavaga@gmail.com>
2021-12-07 20:23:07 -0500

    feat: display delete button only loggged user

--- clase63-security-rules/src/components/molecules/Tweet/Tweet.js
+++ clase63-security-rules/src/components/molecules/Tweet/Tweet.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
 import { ReactComponent as IconHeart } from "../../../assets/icons/iconHeart.svg";
+import { UserContext } from "../../../contexts/UserContext";
 import useTweetCollection from "../../../hooks/useTweetCollection";
 import "./styles.css";
 
 const Tweet = ({ tweetData }) => {
   const { deleteTweet, addLikes } = useTweetCollection();
-  const { tweet: text, author, id, likes } = tweetData;
+  const { tweet: text, author, id, uid, likes } = tweetData;
+
+  const { user } = useContext(UserContext);
 
   const handleAddLikes = async (id) => {
     await addLikes(id);
@@ -19,7 +22,9 @@ const Tweet = ({ tweetData }) => {
           <b>Author:</b> {author}
         </span>
         <div className="tweetFooter">
-          <button onClick={() => deleteTweet(id)}>borrar</button>
+          {uid === user?.uid && (
+            <button onClick={() => deleteTweet(id)}>borrar</button>
+          )}
           <div className="likesWrapper" onClick={() => handleAddLikes(id)}>
             <IconHeart className="icon" /> {likes && `+${likes}`}
           </div>
