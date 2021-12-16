39815189b1a737a923a46e0013f9068d05c267d9
Jose D. Vasquez <jodavaga@gmail.com>
2021-12-16 18:01:48 -0500

    feat: clase65

--- clase65-Intro-testing/src/App.js
+++ clase65-Intro-testing/src/App.js
@@ -18,6 +18,33 @@ function App() {
 
   const { logout } = useAuthentication();
 
+  // Toolbox
+
+  const esImpar = (num) => (num % 2 === 0 ? false : true);
+  const esPar = (num) => (num % 2 === 0 ? true : false);
+
+  const validarEsImpar = () => {
+    // caso 1 (happy path)
+    console.assert(
+      esImpar(3) === false,
+      `<Validando esIMPAR()> - El numero no es IMPAR, es par: ${3}`
+    );
+    // caso de uso 2
+    console.assert(
+      esImpar(6),
+      `<Validando esIMPAR()> - El numero no es IMPAR, es par: ${6}`
+    );
+  };
+  const validarEsPar = (num) => {
+    console.assert(
+      esPar(num),
+      `<Validando esPAR()> El numero es IMPAR: ${num}`
+    );
+  };
+
+  validarEsImpar();
+  validarEsPar(1);
+
   return (
     <div className="App">
       <h1>Clase 65 - Intro Testing</h1>
